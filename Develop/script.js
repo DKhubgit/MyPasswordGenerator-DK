4aac0a10afe0450d7db7138e9c7833977d351818
DanielK <Danielkang13@gmail.com>
2022-06-04 15:09:25 -0700

    removed confirmation in length prompt and added it into generatePassword function, created prompts to have user choose special characters to include into password

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,7 @@
 // Assignment code here
 
+var askAgain;
+
 function generatePassword() {
   var password = "Password not generated"
   var numLength;
@@ -7,33 +9,55 @@ function generatePassword() {
   var criteria = prompt(
     "Please type the following Number to include in your password:\n1. Length of the password\n2. Character types");
     if (criteria == 1) {
-        numChar = wordLength();
-        if (numChar === 0) {
-          generatePassword();
-        }
+      numChar = wordLength();
+    } else if (criteria == 2) {
+      specialChar = wordTypes();
+    } else {
+      return password;
     }
-  return password;
+
+    askAgain = confirm("Would you like to add another criteria?");
+    if (askAgain) {
+      generatePassword();
+    } else {
+      return password;
+    }
 }
 
+//prompt for choosing the length of the password, returns a number.
 function wordLength() {
-   var numLength = prompt("Please choose a length of at least 8 characters and no more than 128 characters");
-   if (numLength == null) {
-      var answer = confirm("Would you like to start from the beginning?");
-        if (answer) {
-          generatePassword();
-        } else {
-          return;
-        }
-   } else if (numLength == "") {
+   var userLength = prompt("Please choose a length of at least 8 characters and no more than 128 characters");
+   if (userLength == null) {
+      userLength = 0;
+      return userLength;
+   } else if (userLength == "") {
       alert("Please choose a number from 8 to 128");
       wordLength();
-   } else if (numLength < 8 || numLength > 128) {;
+   } else if (userLength < 7 || userLength > 126) {;
       alert("Please choose a number from 8 to 128");
       wordLength();
-  } else {
-      return numLength;
+   } else {
+     return userLength;
+   }
+}
+
+//prompt for choosing whether or not to include special character types. Returns an array.
+function wordTypes() {
+  var typesList = [
+    confirm("Would you like to include 'LOWERCASE' letters?\n 'YES' press 'OK || 'NO' press 'cancel'"),
+    confirm("Would you like to include 'UPPERCASE' letters\n 'YES' press 'OK || 'NO' press 'cancel'"),
+    confirm("Would you like to include 'NUMERIC' values\n 'YES' press 'OK || 'NO' press 'cancel'"),
+    confirm("Would you like to include 'SPECIAL CHARACTERS'?\n 'YES' press 'OK || 'NO' press 'cancel'")
+  ]
+  var userTypes;
+  var userTypesList = [0, 0, 0, 0];
+  for (var i = 0; i < typesList.length; ++i) {
+    userTypes = typesList[i];
+    if (userTypes) {
+      userTypesList[i] = 1;
+    }
   }
-  return numLength;
+  return userTypesList;
 }
 
 
